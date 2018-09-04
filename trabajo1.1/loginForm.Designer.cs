e90e624b64f516fc7ba264400b9f9769bf1fbe7f
PincheHaro <harrotl@hotmail.com>
2018-09-04 12:18:46 -0500

    formprron

---
+++ trabajo1.1/loginForm.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace trabajo1._1
+{
+    partial class loginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textNombre = new System.Windows.Forms.TextBox();
+            this.textcontrana = new System.Windows.Forms.TextBox();
+            this.BtnIniciar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(321, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(321, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Contrasena";
+            // 
+            // textNombre
+            // 
+            this.textNombre.Location = new System.Drawing.Point(149, 40);
+            this.textNombre.Name = "textNombre";
+            this.textNombre.Size = new System.Drawing.Size(392, 20);
+            this.textNombre.TabIndex = 2;
+            // 
+            // textcontrana
+            // 
+            this.textcontrana.Location = new System.Drawing.Point(149, 126);
+            this.textcontrana.Name = "textcontrana";
+            this.textcontrana.Size = new System.Drawing.Size(392, 20);
+            this.textcontrana.TabIndex = 3;
+            // 
+            // BtnIniciar
+            // 
+            this.BtnIniciar.Location = new System.Drawing.Point(580, 198);
+            this.BtnIniciar.Name = "BtnIniciar";
+            this.BtnIniciar.Size = new System.Drawing.Size(84, 34);
+            this.BtnIniciar.TabIndex = 4;
+            this.BtnIniciar.Text = "Ingresar";
+            this.BtnIniciar.UseVisualStyleBackColor = true;
+            this.BtnIniciar.Click += new System.EventHandler(this.BtnIniciar_Click);
+            // 
+            // loginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(702, 253);
+            this.Controls.Add(this.BtnIniciar);
+            this.Controls.Add(this.textcontrana);
+            this.Controls.Add(this.textNombre);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "loginForm";
+            this.Text = "loginForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textNombre;
+        private System.Windows.Forms.TextBox textcontrana;
+        private System.Windows.Forms.Button BtnIniciar;
+    }
+}
